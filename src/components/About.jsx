0e8d18c056c275e0910d81a0e9c1244c263ae7e5
nem <nem.alt001@gmail.com>
2020-03-30 11:46:27 -0600

    About Me Image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import profile from '../assets/profile.png';
 
 function About() {
 
     return (
         <div className = "about-container">
-            <div className = "about-section flex-about">
+            <div className = "about-section">
+                <div className = "profile-image">
+                    <Image src={profile} roundedCircle/>
+                </div>
             </div>
         </div>
     );
