468136a6caf5a8dabeaff019de54ad2d05872d97
nem <nem.alt001@gmail.com>
2020-03-30 11:15:47 -0600

    Added About Me Container

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <div className = "about-container">
+            <div className = "about-section flex-about">
+            </div>
+        </div>
+    );
+
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import About from './About';
 
 function App() {
   return (
     <div>
         <Header />
+        <About />
     </div>
   );
 }
