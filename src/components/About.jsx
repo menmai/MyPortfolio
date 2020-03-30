646239207b29e3d62a54e44e3685f2d1cc3eb516
nem <nem.alt001@gmail.com>
2020-03-30 12:11:39 -0600

    Added text section to About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,11 @@ function About() {
                     <div className = "profile-image">
                         <Image src={profile} roundedCircle/>
                     </div>
-                    
+                    <div className = "about-text">
+                        <h1>About Me</h1>
+                        <hr/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis faucibus turpis, vel sodales eros feugiat sed. Proin malesuada sit amet sapien a tristique. Mauris erat mi, vestibulum sit amet molestie nec, pretium eu risus. Donec congue erat non lorem fringilla elementum. Sed ac lacus arcu. Ut sit amet dolor tellus. Duis sapien ligula, rhoncus et fringilla id, congue ullamcorper enim. Ut nec ex ipsum. Vivamus mattis lacinia erat in imperdiet. Cras sed dui vitae lacus consequat bibendum ac euismod lorem.</p>
+                    </div>
                 </div>
             </div>
         </div>
