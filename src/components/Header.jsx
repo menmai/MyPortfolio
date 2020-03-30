f5cb3f75221d41724b14bfe47f531aedd37369be
nem <nem.alt001@gmail.com>
2020-03-30 10:08:01 -0600

    Added Hero Background Image

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div className="hero flex-center">
+            <div className="hero-message">
+                <h1 className="hero-title">Title</h1>
+                <h2 className="hero-sub-title">Subtitle</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
