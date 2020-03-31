8753f95205af2b59d5cd91c5d9e72830633d8466
nem <nem.alt001@gmail.com>
2020-03-31 09:45:39 -0600

    Renamed Header -> Main & Added title effects

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Main from './Main';
 import About from './About';
 import Portfolio from './Portfolio';
 import Navigation from './Navigation';
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
         <Navigation />
-        <Header />
+        <Main />
         <About />
         <Portfolio />
     </div>
---
+++ src/components/Main.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import TitleTransition from './TitleTransition';
+
+function Main() {
+
+    const [titleLoaded, setTitleLoaded] = useState(false);
+
+    function handleLoad() {
+        setTitleLoaded(true);
+    }
+
+    const title = "Title";
+    const subTitle = "Subtitle"
+
+    return(
+        <div id="home" className="hero flex-center">
+            <div className="hero-message">
+                <TitleTransition
+                    elem={<h1 className="hero-title">{title}</h1>}
+                    onEntered={handleLoad}
+                />
+                {titleLoaded ?
+                    <TitleTransition
+                        elem={<h2 className="hero-sub-title">{subTitle}</h2>}
+                    />
+                : <h2 className="hero-sub-title" style={{opacity:0}}>{subTitle}</h2>}
+            </div>
+        </div>
+    );
+}
+
+export default Main;
