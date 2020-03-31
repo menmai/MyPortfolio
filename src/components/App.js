875db8a774f2752b7d551da115bde1772c6e78fd
nem <nem.alt001@gmail.com>
2020-03-30 19:34:06 -0600

    Added Navigation Bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from './Header';
 import About from './About';
 import Portfolio from './Portfolio';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div>
+        <Navigation />
         <Header />
         <About />
         <Portfolio />
