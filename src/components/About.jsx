ed9f44476d9e1b9113099f6a02d0ba5609e62fa3
nem <nem.alt001@gmail.com>
2020-03-31 10:36:29 -0600

    Fixed eslint no-multi-str

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ import profile from '../assets/profile.png';
 function About() {
 
     const sectionTitle = "About Me";
-    const aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis faucibus turpis, \
-        vel sodales eros feugiat sed. Proin malesuada sit amet sapien a tristique. Mauris erat mi, vestibulum \
-        sit amet molestie nec, pretium eu risus. Donec congue erat non lorem fringilla elementum. Sed ac lacus \
-        arcu. Ut sit amet dolor tellus. Duis sapien ligula, rhoncus et fringilla id, congue ullamcorper enim. \
-        Ut nec ex ipsum. Vivamus mattis lacinia erat in imperdiet. Cras sed dui vitae lacus consequat bibendum \
-        ac euismod lorem.";
+    const aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis faucibus turpis, \n" +
+        "vel sodales eros feugiat sed. Proin malesuada sit amet sapien a tristique. Mauris erat mi, vestibulum \n" +
+        "sit amet molestie nec, pretium eu risus. Donec congue erat non lorem fringilla elementum. Sed ac lacus \n" +
+        "arcu. Ut sit amet dolor tellus. Duis sapien ligula, rhoncus et fringilla id, congue ullamcorper enim. \n" +
+        "Ut nec ex ipsum. Vivamus mattis lacinia erat in imperdiet. Cras sed dui vitae lacus consequat bibendum \n" +
+        "ac euismod lorem.";
 
     return (
         <div id="about" className="basic-container">
