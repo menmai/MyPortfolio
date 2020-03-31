b0da1f5a6436adb6fa59c5b6edcec5ab60110f22
nem <nem.alt001@gmail.com>
2020-03-31 10:22:39 -0600

    Extracted modifiable text to constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,14 @@ import profile from '../assets/profile.png';
 
 function About() {
 
+    const sectionTitle = "About Me";
+    const aboutText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis faucibus turpis, \
+        vel sodales eros feugiat sed. Proin malesuada sit amet sapien a tristique. Mauris erat mi, vestibulum \
+        sit amet molestie nec, pretium eu risus. Donec congue erat non lorem fringilla elementum. Sed ac lacus \
+        arcu. Ut sit amet dolor tellus. Duis sapien ligula, rhoncus et fringilla id, congue ullamcorper enim. \
+        Ut nec ex ipsum. Vivamus mattis lacinia erat in imperdiet. Cras sed dui vitae lacus consequat bibendum \
+        ac euismod lorem.";
+
     return (
         <div id="about" className="basic-container">
             <div className="about-section">
@@ -12,9 +20,9 @@ function About() {
                         <Image src={profile} roundedCircle/>
                     </div>
                     <div className = "about-text">
-                        <h1>About Me</h1>
+                        <h1>{sectionTitle}</h1>
                         <hr/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis faucibus turpis, vel sodales eros feugiat sed. Proin malesuada sit amet sapien a tristique. Mauris erat mi, vestibulum sit amet molestie nec, pretium eu risus. Donec congue erat non lorem fringilla elementum. Sed ac lacus arcu. Ut sit amet dolor tellus. Duis sapien ligula, rhoncus et fringilla id, congue ullamcorper enim. Ut nec ex ipsum. Vivamus mattis lacinia erat in imperdiet. Cras sed dui vitae lacus consequat bibendum ac euismod lorem.</p>
+                        <p>{aboutText}</p>
                     </div>
                 </div>
             </div>
