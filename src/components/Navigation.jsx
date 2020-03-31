8ff9b16aa6f1e08d42cd991d02772b7031c9c096
nem <nem.alt001@gmail.com>
2020-03-30 19:41:43 -0600

    Added Nav Buttons & Fixed Padding

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,7 +10,9 @@ function Navigation() {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
