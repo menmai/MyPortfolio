8cc67749125758f29a3d7e7401b3fd5cab421e50
nem <nem.alt001@gmail.com>
2020-03-31 10:24:20 -0600

    Extracted modifiable text to constants

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 function Portfolio() {
 
+    const sectionTitle = "PORTFOLIO";
+    const sectionSubTitle = "View My Recent Projects";
+
     return (
         <div id="portfolio" className="basic-container">
             <div className="portfolio-section">
-                <h1>PORTFOLIO</h1>
-                <p>View My Recent Projects</p>
+                <h1>{sectionTitle}</h1>
+                <p>{sectionSubTitle}</p>
                 <hr/>
                 <div className="flex-wrapped">
 
